a27361a7de7657980da798ed5e0f107e89402241
Imran Khan <imran995353@gmail.com>
2022-01-28 12:32:52 +0530

    I added a function that gets the sum of values from 1 to n

--- src/Recursion.java
+++ src/Recursion.java
@@ -17,6 +17,13 @@ public class Recursion {
         System.out.println("Num Leaves: " + numLeaves(node));
     }
 
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+
     public static int sumDigits(int n) {
         if (n == 0) {
             return 0;
